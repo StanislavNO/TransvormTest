75b08ec249df2b53f912890b12f3281419d25b8b
Stanislav N <stanislav_developer@mail.ru>
2023-09-15 20:29:14 +0500

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward *
+            _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Scaling.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Scaling : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Vector3 _startScale;
+    private Vector3 _endScale;
+    private bool _isGrowing = true;
+
+    private void Start()
+    {
+        _startScale = transform.localScale;
+        _endScale = _startScale * 2;
+    }
+
+    private void Update()
+    {
+        if (_isGrowing)
+        {
+            transform.localScale += Vector3.one * _speed * Time.deltaTime;
+
+            if (transform.localScale.magnitude >= _endScale.magnitude)
+                _isGrowing = false;
+        }
+        else
+        {
+            transform.localScale -= Vector3.one * _speed * Time.deltaTime;
+
+            if (transform.localScale.magnitude <= _startScale.magnitude)
+                _isGrowing = true;
+        }
+    }
+}
